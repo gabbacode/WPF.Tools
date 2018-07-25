dc4dd95fbc11261309db1392f407f1661ec995e5
Лаврентий Афанасьев <lavr.afanasiev_reg@mail.ru>
2018-07-25 11:43:58 +0300

    adding validation support fluentValidation

--- Kanban.Desktop/Ui.Wpf.Common/ViewModels/ViewModelBase.cs
+++ Kanban.Desktop/Ui.Wpf.Common/ViewModels/ViewModelBase.cs
@@ -2,10 +2,13 @@
 using ReactiveUI.Fody.Helpers;
 using System;
 using System.Reactive.Disposables;
+using System.ComponentModel;
+using FluentValidation;
+using System.Linq;
 
 namespace Ui.Wpf.Common.ViewModels
 {
-    public abstract class ViewModelBase : ReactiveObject, IViewModel
+    public abstract class ViewModelBase : ReactiveObject, IViewModel, IDataErrorInfo
     {
         [Reactive]
         public string Title { get; set; }
@@ -47,5 +50,41 @@ namespace Ui.Wpf.Common.ViewModels
         #endregion
 
         protected CompositeDisposable disposables = new CompositeDisposable();
+
+        protected IValidator validator;
+
+        public string Error
+        {
+            get
+            {
+                if (validator != null)
+                {
+                    var results = validator.Validate(this);
+                    if (results != null && results.Errors.Any())
+                    {
+                        var errors = string.Join(Environment.NewLine, results.Errors.Select(x => x.ErrorMessage).ToArray());
+                        return errors;
+                    }
+                }
+                return string.Empty;
+            }
+        }
+
+
+        public string this[string columnName]
+        {
+            get
+            {
+                if (validator != null)
+                {
+                    var firstOrDefault = validator.Validate(this).Errors.FirstOrDefault(lol => lol.PropertyName == columnName);
+                    if (firstOrDefault != null)
+                        return validator != null ? firstOrDefault.ErrorMessage : "";
+                }
+                return "";
+            }
+        }
+
+
     }
 }
